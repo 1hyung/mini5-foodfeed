222b320e9eeee32a0d653a6974ffd3e6413b5abc
1hyung <ryu1hyung@naver.com>
2024-05-27 21:27:36 +0900

    build: Add springdoc-openapi-starter-webmvc-ui dependency
    - Added springdoc-openapi-starter-webmvc-ui version 2.2.0 to build.gradle
    - Enabled OpenAPI documentation and Swagger UI for the project

--- src/main/kotlin/com/teamsparta/mini5foodfeed/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/infra/swagger/SwaggerConfig.kt
@@ -1,4 +1,13 @@
 package com.teamsparta.mini5foodfeed.infra.swagger
 
+import io.swagger.v3.oas.models.OpenAPI
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class SwaggerConfig {
+    @Bean
+    fun openAPI(): OpenAPI {
+        return OpenAPI()
+    }
 }
