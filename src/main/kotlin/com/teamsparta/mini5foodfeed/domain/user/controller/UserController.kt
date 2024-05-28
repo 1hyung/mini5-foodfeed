5e63c54ff50486b30504aa0128f7269b21320721
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-28 15:22:14 +0900

    feat: 유저 컨트롤러 / 회원가입, 프로필수정 추가

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/controller/UserController.kt
@@ -3,21 +3,30 @@ package com.teamsparta.mini5foodfeed.domain.user.controller
 import com.teamsparta.mini5foodfeed.domain.user.dto.request.SignUpRequest
 import com.teamsparta.mini5foodfeed.domain.user.dto.request.UpdateUserProfileRequest
 import com.teamsparta.mini5foodfeed.domain.user.dto.response.UserResponse
+import com.teamsparta.mini5foodfeed.domain.user.service.UserService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-class UserController {
+class UserController(
+    val userService: UserService
+) {
 
     @PostMapping("/signup")
     fun signUp(@RequestBody signUpRequest: SignUpRequest): ResponseEntity<UserResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(userService.signUp(signUpRequest))
     }
 
     @PutMapping("/users/{userId}/profile")
-    fun updateUserProfile(@PathVariable userId: Long,
-                          @RequestBody updateUserProfileRequest: UpdateUserProfileRequest
+    fun updateUserProfile(
+        @PathVariable userId: Long,
+        @RequestBody updateUserProfileRequest: UpdateUserProfileRequest
     ): ResponseEntity<UserResponse> {
-        TODO()
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(userService.updateUserProfile(userId,updateUserProfileRequest))
     }
 }
