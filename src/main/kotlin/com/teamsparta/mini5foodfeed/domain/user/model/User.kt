9bf9159464da1c0bbef74e3e4c6c890edc4fc50a
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-28 15:37:44 +0900

    feat : 회원가입, 프로필 수정 비즈니스 로직 1차 작성 및 유저 엔티티 생성

---
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/model/User.kt
@@ -0,0 +1,29 @@
+package com.teamsparta.mini5foodfeed.domain.user.model
+
+import com.teamsparta.mini5foodfeed.domain.user.dto.response.UserResponse
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity
+class User(
+    @Column
+    var userId: String,
+    @Column
+    var userName: String,
+    @Column
+    var password: String
+) {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null
+}
+fun User.toResponse(): UserResponse {
+    return UserResponse(
+        id = id!!,
+        userId = userId,
+        userName = userName
+    )
+}
