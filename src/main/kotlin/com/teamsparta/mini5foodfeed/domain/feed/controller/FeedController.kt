08657f9b8b524b687de20ec25fc26556b6b55c64
pcouqc <nagapen@nate.com>
2024-05-28 14:18:34 +0900

    feat : 컨트롤러 구현(초기버전)

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/controller/FeedController.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/controller/FeedController.kt
@@ -1,4 +1,65 @@
 package com.teamsparta.mini5foodfeed.domain.feed.controller
 
-class FeedController {
+import com.teamsparta.mini5foodfeed.domain.feed.dto.*
+import com.teamsparta.mini5foodfeed.domain.feed.service.FeedService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+
+@RequestMapping("/feeds")
+@RestController
+class FeedController(
+    private val feedService: FeedService
+) {
+
+    @GetMapping("/cursor")
+    fun getFeedList(
+        @RequestParam(required = false) tags: List<String>?,
+        @RequestParam(required = false) author: String?,
+        @RequestParam cursorRequest: CursorRequest,
+        @RequestParam(required = false, defaultValue = "20") size: Int,
+    ): ResponseEntity<CursorPage<FeedResponseDto>> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(feedService.getFeedList(tags, author, cursorRequest))
+    }
+
+
+    @GetMapping("/{feedId}")
+    fun getFeedDetails(
+        @PathVariable("feedId") feedId : Long,
+    ) : ResponseEntity<FeedResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(feedService.getFeedDetail(feedId))
+    }
+
+    @PostMapping
+    fun createFeed(
+        @RequestBody request: CreateRequestDto
+    ) : ResponseEntity<FeedResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .body(feedService.createFeed(request))
+    }
+
+    @PutMapping("/{feedId}")
+    fun updateFeed(
+        @PathVariable("feedId") feedId: Long,
+        @RequestBody request: UpdateRequestDto
+    ) : ResponseEntity<FeedResponseDto> {
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(feedService.updateFeed(request))
+    }
+
+    @DeleteMapping("/{feedId}")
+    fun deleteFeed(
+        @PathVariable("feedId") feedId: Long,
+    ) : ResponseEntity<Unit> {
+        return ResponseEntity
+            .status(HttpStatus.NO_CONTENT)
+            .build()
+    }
 }
