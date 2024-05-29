0d256b3fe438e69a27b968a48a69f4e1fef64d5f
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-29 16:13:47 +0900

    feat : 로그인 관련 기본 구조 생성, 회원가입 서비스 로직 수정

---
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/dto/request/LoginRequest.kt
@@ -0,0 +1,6 @@
+package com.teamsparta.mini5foodfeed.domain.user.dto.request
+
+data class LoginRequest(
+    val userId: String,
+    val password: String
+)
--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/service/UserService.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/service/UserService.kt
@@ -1,14 +1,15 @@
 package com.teamsparta.mini5foodfeed.domain.user.service
 
+import com.teamsparta.mini5foodfeed.domain.user.dto.request.LoginRequest
 import com.teamsparta.mini5foodfeed.domain.user.dto.request.SignUpRequest
 import com.teamsparta.mini5foodfeed.domain.user.dto.request.UpdateUserProfileRequest
 import com.teamsparta.mini5foodfeed.domain.user.dto.response.UserResponse
-import com.teamsparta.mini5foodfeed.domain.user.exception.userIdIllegalStateException
+import com.teamsparta.mini5foodfeed.domain.user.exception.UserIdIllegalStateException
+import com.teamsparta.mini5foodfeed.domain.user.exception.UserIdNotFoundException
 import com.teamsparta.mini5foodfeed.domain.user.model.User
 import com.teamsparta.mini5foodfeed.domain.user.model.toResponse
 import com.teamsparta.mini5foodfeed.domain.user.repository.UserRepository
 import com.teamsparta.mini5foodfeed.exception.NotFoundException
-import com.teamsparta.mini5foodfeed.exception.userNameIllegalStateException
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -19,11 +20,8 @@ class  UserService(
 ) {
 
     fun signUp(request: SignUpRequest): UserResponse {
-        // TODO: userId이 중복되는지 확인, 중복된다면 throw IllegalStateException
         if(userRepository.existsByUserId(request.userId)){
-            throw userIdIllegalStateException(request.userId)
-        }else if(userRepository.existsByUserName(request.userName)){
-            throw userNameIllegalStateException(request.userName)
+            throw UserIdIllegalStateException(request.userId)
         }
         val user = User(
             userId = request.userId,
@@ -31,7 +29,14 @@ class  UserService(
             password = request.password
         )
         return userRepository.save(user).toResponse()
-        // TODO: request를 User로 변환 후 DB에 저장, 비밀번호는 저장시 암호화
+    }
+
+    fun logIn(request: LoginRequest): UserResponse{
+        if(!userRepository.existsByUserId(request.userId)){
+            throw UserIdNotFoundException(request.userId)
+        }
+        val user = userRepository.findByUserId(request.userId)
+        return user.toResponse()
     }
 
     @Transactional
@@ -39,7 +44,5 @@ class  UserService(
         val user = userRepository.findByIdOrNull(userId) ?: throw NotFoundException(userId)
         user.userName = request.userName
         return userRepository.save(user).toResponse()
-        // TODO: 만약 userId에 해당하는 User가 없다면 throw ModelNotFoundException
-        // TODO: DB에서 userId에 해당하는 User를 가져와서 updateUserProfileRequest로 업데이트 후 DB에 저장, 결과를 UserResponse로 변환 후 반환
     }
 }
