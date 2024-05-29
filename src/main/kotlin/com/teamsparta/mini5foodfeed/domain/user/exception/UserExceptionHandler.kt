be4c8c62b23cee6724d3b669f55d26acbc2ed563
Kozy5 <96171308+Kozy5@users.noreply.github.com>
2024-05-29 16:14:42 +0900

    refactor : 유저 아이디 관련 예외처리 추가 및 오타 수정

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/exception/UserExceptionHandler.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/exception/UserExceptionHandler.kt
@@ -1,14 +1,21 @@
 package com.teamsparta.mini5foodfeed.exception
 
-import com.teamsparta.mini5foodfeed.domain.user.exception.userIdIllegalStateException
+import com.teamsparta.mini5foodfeed.domain.user.exception.UserIdIllegalStateException
+import com.teamsparta.mini5foodfeed.domain.user.exception.UserIdNotFoundException
 import com.teamsparta.mini5foodfeed.exception.dto.ErrorResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 class UserExceptionHandler{
-    @ExceptionHandler(userNameIllegalStateException::class, userIdIllegalStateException::class)
-    fun handleNotFoundException(e: Exception): ResponseEntity<ErrorResponse>{
+    @ExceptionHandler(UserIdIllegalStateException::class)
+    fun handleUserIdNotExistException(e: Exception): ResponseEntity<ErrorResponse>{
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(ErrorResponse(message = e.message))
+    }
+    @ExceptionHandler(UserIdNotFoundException::class)
+    fun handleUserIdNotFoundException(e: Exception): ResponseEntity<ErrorResponse>{
         return ResponseEntity
             .status(HttpStatus.NOT_FOUND)
             .body(ErrorResponse(message = e.message))
--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/exception/userIdIllegalStateException.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/exception/userIdIllegalStateException.kt
@@ -1,3 +1,3 @@
 package com.teamsparta.mini5foodfeed.domain.user.exception
 
-data class userIdIllegalStateException(val userId:String):RuntimeException("already use in username:$userId")
+data class UserIdIllegalStateException(val userId:String):RuntimeException("already use in username:$userId")
