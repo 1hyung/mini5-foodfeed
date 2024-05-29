2b84f809e4f2f1f518888ec89e5fa48741e479ad
pcouqc <nagapen@nate.com>
2024-05-29 15:14:21 +0900

    feat : 엔티티 연결 변경 이슈로 save 로직 바뀜

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/service/CommentService.kt
@@ -3,19 +3,34 @@ package com.teamsparta.mini5foodfeed.domain.comment.service
 import com.teamsparta.mini5foodfeed.domain.comment.dto.CommentRequest
 import com.teamsparta.mini5foodfeed.domain.comment.dto.CommentResponse
 import com.teamsparta.mini5foodfeed.domain.comment.model.Comment
+import com.teamsparta.mini5foodfeed.domain.comment.model.toResponse
 import com.teamsparta.mini5foodfeed.domain.comment.repository.CommentRepository
+import com.teamsparta.mini5foodfeed.domain.feed.repository.FeedRepository
+import com.teamsparta.mini5foodfeed.exception.ModelNotFoundException
 import jakarta.transaction.Transactional
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 class CommentService(
-    private val commentRepository: CommentRepository
+    private val commentRepository: CommentRepository,
+    private val feedRepository: FeedRepository
 ) {
     @Transactional
     fun createComment(feedId: Long, request: CommentRequest): CommentResponse {
-        val comment = Comment(feedId = feedId, contents = request.contents)
-        commentRepository.save(comment)
-        return CommentResponse(contents = request.contents)
+
+//        val comment = Comment(feedId = feedId, contents = request.contents)
+//        commentRepository.save(comment)
+//        return CommentResponse(contents = request.contents)
+        val feed = feedRepository.findByIdOrNull(feedId) ?: throw ModelNotFoundException ("feed" , feedId)
+        return commentRepository.save(
+            Comment(
+                contents = request.contents,
+                createdAt = LocalDateTime.now(),
+                feed = feed
+            )
+        ).toResponse()
     }
 
     @Transactional
