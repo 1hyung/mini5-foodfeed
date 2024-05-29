f4a7740f966f8991608bd156f298f2d243d790e7
1hyung <ryu1hyung@naver.com>
2024-05-29 09:39:51 +0900

    feat: 재범님 작성 부분 Pull Request

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/service/UserService.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/user/service/UserService.kt
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
-class UserService(
+class  UserService(
     val userRepository: UserRepository,
 ) {
 
