db5a30509b60144e1162f9ee8f4e767f8122f6ec
pcouqc <nagapen@nate.com>
2024-05-29 17:39:12 +0900

    feat : 댓글 반환 dto 추가

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/dto/CommentResponse.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/dto/CommentResponse.kt
@@ -1,5 +1,8 @@
 package com.teamsparta.mini5foodfeed.domain.comment.dto
 
+import java.time.LocalDateTime
+
 data class CommentResponse(
-    val contents: String
+    val contents: String,
+    val createdAt: LocalDateTime
 )
