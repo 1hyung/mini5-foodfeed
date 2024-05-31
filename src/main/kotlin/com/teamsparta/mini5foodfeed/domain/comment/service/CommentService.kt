15b564990b9f5c3f4f3c407ff80a2fb0fc8dd8fa
pcouqc <nagapen@nate.com>
2024-05-31 11:27:54 +0900

    fix : 댓글 인가 과정에서 피드 아이디를 요구하는 버그 수정

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/service/CommentService.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/service/CommentService.kt
@@ -1,20 +1,18 @@
 package com.teamsparta.mini5foodfeed.domain.comment.service
 
-import com.teamsparta.mini5foodfeed.common.dto.CustomUser
+import com.teamsparta.mini5foodfeed.common.exception.ModelNotFoundException
+import com.teamsparta.mini5foodfeed.common.exception.NotAuthenticationException
 import com.teamsparta.mini5foodfeed.domain.comment.dto.CommentRequest
 import com.teamsparta.mini5foodfeed.domain.comment.dto.CommentResponse
 import com.teamsparta.mini5foodfeed.domain.comment.model.Comment
 import com.teamsparta.mini5foodfeed.domain.comment.model.toResponse
 import com.teamsparta.mini5foodfeed.domain.comment.repository.CommentRepository
-import com.teamsparta.mini5foodfeed.domain.feed.repository.FeedRepository
-import com.teamsparta.mini5foodfeed.common.exception.ModelNotFoundException
-import com.teamsparta.mini5foodfeed.common.exception.NotAuthenticationException
 import com.teamsparta.mini5foodfeed.domain.feed.model.Feed
+import com.teamsparta.mini5foodfeed.domain.feed.repository.FeedRepository
 import com.teamsparta.mini5foodfeed.domain.user.model.Users
 import com.teamsparta.mini5foodfeed.domain.user.repository.UserRepository
 import jakarta.transaction.Transactional
 import org.springframework.data.repository.findByIdOrNull
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -40,9 +38,8 @@ class CommentService(
 
     fun updateComment(feedId: Long, commentId: Long, request: CommentRequest, userId: Long): CommentResponse {
         val feed = getValidatedFeed(feedId)
-        if (userId != feed.user!!.id)  throw NotAuthenticationException("feed")
-        val comment = commentRepository.findById(commentId)
-            .orElseThrow { RuntimeException("Comment not found") }
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("Comment", commentId)
+        if (userId != comment.user!!.id)  throw NotAuthenticationException("comment")
         comment.contents = request.contents
         commentRepository.save(comment)
         return CommentResponse(contents = request.contents, createdAt = LocalDateTime.now())
@@ -50,9 +47,8 @@ class CommentService(
 
     fun deleteComment(feedId: Long, commentId: Long, userId: Long) {
         val feed = getValidatedFeed(feedId)
-        if (userId != feed.user!!.id)  throw NotAuthenticationException("feed")
-        val comment = commentRepository.findById(commentId)
-            .orElseThrow { RuntimeException("Comment not found") }
+        val comment = commentRepository.findByIdOrNull(commentId) ?: throw ModelNotFoundException("Comment", commentId)
+        if (userId != comment.user!!.id)  throw NotAuthenticationException("comment")
         commentRepository.delete(comment)
     }
 
