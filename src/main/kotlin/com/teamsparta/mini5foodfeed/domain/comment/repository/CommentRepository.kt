dc403a31be4403c1b1747c289285dd390786ace6
pcouqc <nagapen@nate.com>
2024-05-31 13:13:49 +0900

    fix : 목록조회시 댓글 미반환 오류 수정

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/comment/repository/CommentRepository.kt
@@ -1,7 +1,6 @@
 package com.teamsparta.mini5foodfeed.domain.comment.repository
 
 import com.teamsparta.mini5foodfeed.domain.comment.model.Comment
-import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -12,6 +11,4 @@ interface CommentRepository : JpaRepository<Comment, Long> {
 
     @Query("SELECT c FROM Comment c LEFT JOIN FETCH c.feed f WHERE f.id = :feedId ORDER BY c.createdAt DESC")
     fun findTop5ByFeedIdOrderByCreatedAtDesc(feedId: Long?, pageable: Pageable): List<Comment>
-
-    fun findByFeedIdOrderByCreatedAtDesc(feedId: Long?): List<Comment>
 }
