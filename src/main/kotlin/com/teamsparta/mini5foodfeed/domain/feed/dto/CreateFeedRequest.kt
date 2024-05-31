64fdcacf2437441b54f8d7207d0719ad3bf45a9b
pcouqc <nagapen@nate.com>
2024-05-31 21:24:24 +0900

    feat : dto에 validation 추가

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/dto/CreateFeedRequest.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/dto/CreateFeedRequest.kt
@@ -1,7 +1,13 @@
 package com.teamsparta.mini5foodfeed.domain.feed.dto
 
+import jakarta.validation.constraints.Size
+
 data class CreateFeedRequest(
+
+    @Size(min = 1 , max = 100, message = "제목은 1 ~ 100 자 이내여야 합니다")
     val title: String,
+    @Size(min = 1 , max = 2000, message = "본문은 1 ~ 2000 자 이내여야 합니다")
     val description: String,
-    val tagVo: TagVo
+    val tagVo: TagVo,
+    val video: String
 )
