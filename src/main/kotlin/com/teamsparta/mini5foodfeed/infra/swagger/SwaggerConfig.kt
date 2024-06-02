696e19c8a1bff030a0bb805746de39981abdd335
pcouqc <nagapen@nate.com>
2024-06-02 22:00:20 +0900

    refactor : swagger 테스트를 위한 설정

--- src/main/kotlin/com/teamsparta/mini5foodfeed/infra/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/infra/swagger/SwaggerConfig.kt
@@ -1,13 +1,30 @@
 package com.teamsparta.mini5foodfeed.infra.swagger
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirements
+import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SwaggerConfig {
+
     @Bean
-    fun openAPI(): OpenAPI {
-        return OpenAPI()
-    }
+    fun openAPI(): OpenAPI = OpenAPI()
+        .components(Components()
+            .addSecuritySchemes("authorization", SecurityScheme()
+                .name("authorization")
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")))
+        .addSecurityItem(SecurityRequirement().addList("authorization"))
+        .info(
+            Info()
+                .title("2Do API")
+                .description("food feed")
+                .version("1.0.0")
+        )
 }
