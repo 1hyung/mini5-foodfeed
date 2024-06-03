6a6b77e3c55777231b2d977eaad4d5fbc2fd9544
LSH <fhzktm@naver.com>
2024-06-03 09:14:04 +0900

    refactor : 조회 필터링 api 의 쿼리메서드 구조 변경, fetch join 으로 N+1문제 해결

--- src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/service/FeedService.kt
+++ src/main/kotlin/com/teamsparta/mini5foodfeed/domain/feed/service/FeedService.kt
@@ -81,7 +81,8 @@ class FeedService(
                 createdAt = LocalDateTime.now(),
                 comments = null,
                 user = user,
-                tag =tag
+                tag =tag,
+                imageUrl = feedRequest.video
             )
         )
         feed.tag.feed = feed
